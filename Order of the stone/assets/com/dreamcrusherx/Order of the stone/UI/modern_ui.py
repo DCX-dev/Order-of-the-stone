6c22b899a11e192f847b417d4296393fb3b085f3
DCX-dev <xavierlahens@gmail.com>
2025-10-09 19:35:00 -0700

    Update version to 1.0 Beta in title screen and credits

--- Order of the stone/assets/com/dreamcrusherx/Order of the stone/UI/modern_ui.py
+++ Order of the stone/assets/com/dreamcrusherx/Order of the stone/UI/modern_ui.py
@@ -147,7 +147,7 @@ class ModernUI:
             button_states[action] = btn_rect
         
         # Version info
-        version_text = self.small_font.render("v1.0 - Modern UI Edition", True, self.colors["text_dim"])
+        version_text = self.small_font.render("v1.0 Beta - Modern UI Edition", True, self.colors["text_dim"])
         self.screen.blit(version_text, (10, self.screen.get_height() - 30))
         
         # Copyright text - Bottom right corner
@@ -502,7 +502,7 @@ class ModernUI:
             "• Python Developers",
             "• All Beta Testers",
             "",
-            "Version 1.0.0",
+            "Version 1.0 Beta",
             "© 2025 Team Banana Labs"
         ]
         
